f40ec6d8cb57f5ba51d9bdf7ed73f90ae7ab3737
emma673r <90177139+emma673r@users.noreply.github.com>
2021-12-07 21:13:04 +0100

    Burger menu V1 (kun i behandlinger indtil videre)

--- script_burger.js
+++ script_burger.js
@@ -1,19 +1,23 @@
-let burger = document.querySelector("#burger_menu");
-let burgerIcon = document.querySelector("#burger_icon");
+const menu = document.querySelector(".burger_menu");
+const menuItems = document.querySelectorAll("li");
+const hamburger = document.querySelector("#hamburger");
+const closeIcon = document.querySelector("#burger_opened");
+const menuIcon = document.querySelector("#burger_closed");
 
-//function burgerMenu()
-function burgerMenu() {
-  console.log("burgerMenu");
-//   document.burger.style.display = "none";
-//   document.burgerIcon.style.display = "block";
-  document
-    .querySelector("#burger_icon")
-    .addEventListener("click", showBurgerMenu);
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
 }
 
-function showBurgerMenu() {
-  console.log("showBurgerMenu");
-//   document.burgerIcon.addEventListener("click", burgerMenu);
-//   document.burger.style.display = "block";
-//   document.burgerIcon.style.display = "none";
-}
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(function (li) {
+  li.addEventListener("click", toggleMenu);
+});
